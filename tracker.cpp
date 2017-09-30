3a6b35e1e3d553252606555a34c49ff8b44dbc55
ypbwith <yangpengbo2636@gmail.com>
2017-09-30 14:18:05 +0800

    Update tracker.cpp

--- tracker.cpp
+++ tracker.cpp
@@ -1,97 +1,156 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/tracking.hpp>
 #include <opencv2/core/ocl.hpp>
+#include <dlib/opencv.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+#include <dlib/gui_widgets.h>
+#include <iostream>
+#include <math.h>
+#include <stddef.h>
 
 
 using namespace cv;
 using namespace std;
 
-// Convert to string
-#define SSTR( x ) static_cast< std::ostringstream & >( \
-( std::ostringstream() << std::dec << x ) ).str()
 
+// Specifying minimum and maximum size parameters
+#define MIN_FACE_SIZE 24
+#define MAX_FACE_SIZE 120
+
+#define SKIPFRAME 5
+#define RATIO 1
 
 
 int main(int argc, char **argv)
 {
-    // List of tracker types in OpenCV 3.2
-    // NOTE : GOTURN implementation is buggy and does not work.
-    const char *types[] = {"BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN"};
-    vector <string> trackerTypes(types, std::end(types));
-    
-    // Create a tracker
-    string trackerType = trackerTypes[2];
-    Ptr<Tracker> tracker = Tracker::create(trackerType);
-
-    // Read video
-    VideoCapture video("videos/chaplin.mp4");
-    
-    // Exit if video is not opened
-    if(!video.isOpened())
-    {
-        cout << "Could not read video file" << endl;
-        return 1;
-        
+    std::vector<cv::Point2f> last_object;
+    for (int i = 0; i < 68; ++i) {
+        last_object.push_back(cv::Point2f(0.0, 0.0));
     }
-    
-    // Read first frame
-    Mat frame;
-    bool ok = video.read(frame);
-    
-    // Define initial boundibg box
-    Rect2d bbox(287, 23, 86, 320);
-    
-    // Uncomment the line below to select a different bounding box
-    // bbox = selectROI(frame, false);
 
-    // Display bounding box.
-    rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
-    imshow("Tracking", frame);
-    
-    tracker->init(frame, bbox);
-    
-    while(video.read(frame))
+    try
     {
-     
-        // Start timer
-        double timer = (double)getTickCount();
-        
-        // Update the tracking result
-        bool ok = tracker->update(frame, bbox);
-        
-        // Calculate Frames per second (FPS)
-        float fps = getTickFrequency() / ((double)getTickCount() - timer);
-        
-        
-        if (ok)
-        {
-            // Tracking success : Draw the tracked object
-            rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
-        }
-        else
-        {
-            // Tracking failure detected.
-            putText(frame, "Tracking failure detected", Point(100,80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
-        }
-        
-        // Display tracker type on frame
-        putText(frame, trackerType + " Tracker", Point(100,20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50),2);
-        
-        // Display FPS on frame
-        putText(frame, "FPS : " + SSTR(int(fps)), Point(100,50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
 
-        // Display frame.
-        imshow("Tracking", frame);
-        
-        // Exit if ESC pressed.
-        int k = waitKey(1);
-        if(k == 27)
-        {
-            break;
-        }
+        //-------------------------------------face detector-----------------------------
+        // Load the Cascade Classifier Xml file
+        CascadeClassifier faceCascade("cascade/haarcascade_lefteye_2splits.xml");
 
+        // Create a VideoCapture object
+        VideoCapture cap(0);
+
+        // Check if camera opened successfully
+        if (!cap.open(0)) return 0;
+
+        Mat frame, frameBig, frameGray;
+        //-----------------------------------face detector end------------------------------
+
+        //----------------------------------------tracker------------------------------------
+        // List of tracker types in OpenCV 3.2
+        // NOTE : GOTURN implementation is buggy and does not work.
+        const char *types[] = { "BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN" };
+        std::vector <string> trackerTypes(types, std::end(types));
+
+        // Create a tracker
+        string trackerType = trackerTypes[2];
+        Ptr<Tracker> tracker_0 = Tracker::create(trackerType);
+        Ptr<Tracker> tracker_1 = Tracker::create(trackerType);
+
+        // Define initial boundibg box
+        Rect2d bbox(10, 10,10, 10);
+        Rect2d bbox_0(10, 10,10, 10);
+        Rect2d bbox_1(10, 10,10, 10);
+        Rect2d facebox(10, 10,10, 10);
+
+        int contframe = 0;
+        //----------------------------tracker end------------------------------------------
+        unsigned long im_mum=0;
+
+        Mat grayscale_image, img_adaptive,img_EqualizeHist;
+
+        // Creating vector to store the detected faces' parameters
+        vector<Rect> faces;
+        bool ok_0=0;
+        bool ok_1=0;
+        while (1)
+        {
+            // Reading each frame
+            bool frameRead = cap.read(frameBig);
+
+            // If frame not opened successfully
+            if (!frameRead)
+                break;
+
+
+            cv::resize(frameBig, frame, cv::Size(), 1.0 / RATIO, 1.0 / RATIO);
+
+            dlib::cv_image<dlib::bgr_pixel> cimg(frame);
+            dlib::cv_image<dlib::bgr_pixel> cframeBig(frameBig);
+
+            // Detect faces
+            faceCascade.detectMultiScale(frame, faces, 1.1, 5, 0, Size(MIN_FACE_SIZE, MIN_FACE_SIZE), Size(MAX_FACE_SIZE, MAX_FACE_SIZE* 0.5));
+
+            contframe++;
+            if(contframe > SKIPFRAME)
+                contframe =0;
+
+            //printf("contframe is :%d\n",contframe);
+
+            if(contframe == 0)
+            {
+                // Loop over each detected face
+                for (int i = 0; i < faces.size(); i++)
+                {
+                    // Dimension parameters for bounding rectangle for face
+                    Rect faceRect = faces[i];
+
+                    if(i==0)
+                    {
+                        bbox_0 = faceRect;
+                        tracker_0 = Tracker::create(trackerType);
+                        tracker_0->init(frame, bbox_0);
+                        ok_0 = tracker_0->update(frame, bbox_0);
+
+                    }
+
+                    if(i==1)
+                    {
+                        bbox_1 = faceRect;
+                        tracker_1 = Tracker::create(trackerType);
+                        tracker_1->init(frame, bbox_1);
+                        ok_1=tracker_1->update(frame, bbox_1);
+
+                    }
+
+                }
+            }
+
+            if(ok_0 )
+            {
+                 tracker_0->update(frame, bbox_0);
+                  rectangle(frame,bbox_0, Scalar(128, 255, 0), 2);
+            }
+            if(ok_1 )
+            {
+                    tracker_1->update(frame, bbox_1);
+
+                    rectangle(frame,bbox_1, Scalar(128, 255, 0), 2);
+            }
+            imshow("tracking", frame);
+            // Exit if ESC pressed.
+            int k = waitKey(1);
+            if (k == 27)
+            {
+                break;
+            }
+        }
     }
-    
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+    }
+    getchar();
 
-    
+    return 0;
 }
